7251470266d973068b19a3aa52ab07d2ffbe7dc2
zhengyix <1043293739@qq.com>
2017-06-11 19:16:11 +0800

    this is my first commit

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+//获取图片相关的数据
+var imageDatas = require('../data/imageDatas.json');
+//let yeomanImage = require('../images/yeoman.png');
+
+//将图片名信息转换成URL路径信息
+//var
+var data = (function genImageURL(imageDatasArr){
+	for(var i =0,j = imageDatasArr.length;i<j;i++){
+		var singleImageData = imageDatasArr[i];
+		singleImageData.imageURL = require('../images/'+singleImageData.fileName);
+
+		imageDatasArr[i] = singleImageData;
+	}
+	return imageDatasArr;
+})(imageDatas);
+
+
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+     	<section className="stage">
+     		<section className="img-sec">
+     		</section>
+     		<nav className="controller-nav">
+     		</nav>
+     	</section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
